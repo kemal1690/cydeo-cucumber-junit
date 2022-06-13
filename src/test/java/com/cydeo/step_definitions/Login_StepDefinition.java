6c976254330a64ab7a61cfb5f1a7fce53a33350c
kml <fatih200530@hotmail.com>
2022-06-13 16:13:39 +0300

    Cucumber-junit

---
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinition.java
@@ -0,0 +1,44 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_StepDefinition {
+
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        System.out.println("User enters librarian username");
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("User enters librarian password");
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        System.out.println("User should see the dashboard");
+    }
+
+
+    @When("user enters student username")
+    public void userEntersStudentUsername() {
+        System.out.println("User enters student username");
+    }
+
+    @And("user enters student password")
+    public void userEntersStudentPassword() {
+        System.out.println("User enters student password");
+    }
+
+
+    @When("user enters admin username")
+    public void userEntersAdminUsername() {
+        System.out.println("User enters admin username");
+    }
+
+    @And("user enters admin password")
+    public void userEntersAdminPassword() {
+        System.out.println("User enters admin password");
+    }
+}
